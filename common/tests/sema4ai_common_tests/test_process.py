772950317888ea90ad58aa2bcb25b36d75f33137
Fabio Zadrozny <fabiofz@gmail.com>
2025-01-15 14:28:50 -0300

    wip

--- common/tests/sema4ai_common_tests/test_process.py
+++ common/tests/sema4ai_common_tests/test_process.py
@@ -39,6 +39,7 @@ def test_launch_and_return_future(scenario):
 
     from sema4ai.common.monitor import Monitor
     from sema4ai.common.process import ProcessResultStatus, launch_and_return_future
+    from sema4ai.common.wait_for import wait_for_condition
 
     code = """
 import time
@@ -79,6 +80,5 @@ while True:
         assert result.status == ProcessResultStatus.TIMED_OUT
     else:
         raise ValueError(f"Invalid scenario: {scenario}")
-
     process = future._process_weak_ref()
-    assert process.poll() is not None
+    wait_for_condition(lambda: process.poll() is not None)
