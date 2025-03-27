bb36254184247424c2d26bd908518b506f3310cc
Fabio Zadrozny <fabiofz@gmail.com>
2025-03-27 11:48:52 -0300

    wip

--- .github/workflows/_gen_workflows.py
+++ .github/workflows/_gen_workflows.py
@@ -78,7 +78,7 @@ class BaseTests:
             "ACTION_SERVER_TEST_ACCESS_CREDENTIALS": "${{ secrets.ACTION_SERVER_TEST_ACCESS_CREDENTIALS }}",
             "ACTION_SERVER_TEST_HOSTNAME": "${{ secrets.ACTION_SERVER_TEST_HOSTNAME }}",
         },
-        "run": "uv run --frozen inv test",
+        "run": "uv run --frozen --no-project --no-sync inv test",
     }
 
     @property
@@ -175,12 +175,12 @@ class BaseTests:
         devinstall = {
             "name": "Install project (dev)",
             "if": "contains(matrix.name, '-devmode')",
-            "run": "uv run --frozen inv devinstall",
+            "run": "uv run --frozen --no-project --no-sync inv devinstall",
         }
         install = {
             "name": "Install project (not dev)",
             "if": "contains(matrix.name, '-devmode') == false",
-            "run": "uv run --frozen inv install",
+            "run": "uv run --frozen --no-project --no-sync inv install",
         }
         setup_node = {
             "name": "Setup node",
@@ -201,7 +201,7 @@ class BaseTests:
 
         build_frontend = {
             "name": "Build frontend",
-            "run": "uv run --frozen inv build-frontend",
+            "run": "uv run --frozen --no-project --no-sync inv build-frontend",
             "env": {
                 "CI": True,
                 "NODE_AUTH_TOKEN": "${{ secrets.GH_PAT_READ_PACKAGES }}",
@@ -210,7 +210,7 @@ class BaseTests:
 
         build_oauth2_config = {
             "name": "Build OAuth2 config",
-            "run": "uv run --frozen inv build-oauth2-config",
+            "run": "uv run --frozen --no-project --no-sync inv build-oauth2-config",
             "env": {"GH_TOKEN": "${{ secrets.GH_PAT_GHA_TO_ANOTHER_REPO }}"},
         }
 
@@ -242,19 +242,19 @@ class BaseTests:
 
         install_poetry = {
             "name": "Install poetry",
-            "run": """uv run --frozen python -c "import sys;print(sys.executable)"\nuv run --frozen -m pip install poetry""",
+            "run": """uv tool install poetry@2.1.1""",
         }
 
         install_devutils = {
             "name": "Install devutils requirements",
-            "run": "uv run --frozen -m pip install -r ../devutils/requirements.txt",
+            "run": "uv run --frozen --no-project --no-sync -m pip install -r ../devutils/requirements.txt",
         }
 
         run_lint = {
             "name": "`inv lint`, potentially fixed with `inv pretty`",
             "if": "always()",
             "run": """
-uv run --frozen inv lint
+uv run --frozen --no-project --no-sync inv lint
 """,
         }
 
@@ -262,7 +262,7 @@ uv run --frozen inv lint
             "name": "`inv typecheck`",
             "if": "always()",
             "run": """
-uv run --frozen inv typecheck
+uv run --frozen --no-project --no-sync inv typecheck
 """,
         }
 
@@ -270,7 +270,7 @@ uv run --frozen inv typecheck
             "name": "`inv docs` with checking on files changed",
             "if": "always()",
             "run": """
-uv run --frozen inv docs --check
+uv run --frozen --no-project --no-sync inv docs --check
 """,
         }
 
@@ -278,7 +278,7 @@ uv run --frozen inv docs --check
             "name": "`inv docs --validate`",
             "if": "always()",
             "run": """
-uv run --frozen inv docs --validate
+uv run --frozen --no-project --no-sync inv docs --validate
 """,
         }
 
@@ -359,7 +359,7 @@ class ActionServerTests(BaseTests):
                 "GITHUB_REF_NAME": "${{ github.ref_name }}",
                 "GITHUB_PR_NUMBER": "${{ github.event.pull_request.number }}",
             },
-            "run": "uv run --frozen poetry run inv build-executable --sign --go-wrapper",
+            "run": "uv run --frozen --no-project --no-sync poetry run inv build-executable --sign --go-wrapper",
         },
         {
             "name": "Upload artifact",
@@ -378,7 +378,7 @@ class ActionServerTests(BaseTests):
                 "ACTION_SERVER_TEST_ACCESS_CREDENTIALS": "${{ secrets.ACTION_SERVER_TEST_ACCESS_CREDENTIALS }}",
                 "ACTION_SERVER_TEST_HOSTNAME": "${{ secrets.ACTION_SERVER_TEST_HOSTNAME }}",
             },
-            "run": "uv run --frozen poetry run inv test-not-integration",
+            "run": "uv run --frozen --no-project --no-sync poetry run inv test-not-integration",
         },
         {
             "name": "Test (integration)",
@@ -389,7 +389,7 @@ class ActionServerTests(BaseTests):
                 "ACTION_SERVER_TEST_ACCESS_CREDENTIALS": "${{ secrets.ACTION_SERVER_TEST_ACCESS_CREDENTIALS }}",
                 "ACTION_SERVER_TEST_HOSTNAME": "${{ secrets.ACTION_SERVER_TEST_HOSTNAME }}",
             },
-            "run": "uv run --frozen poetry run inv test-binary --jobs 0",
+            "run": "uv run --frozen --no-project --no-sync poetry run inv test-binary --jobs 0",
         },
     ]
 
