137e727d89019566a78d34f5782f753a25ea944c
Ovidiu Rusu <ovidiu@sema4.ai>
2025-04-01 17:55:42 +0300

    Fix

--- sema4ai-http-helper/src/sema4ai_http/__init__.py
+++ sema4ai-http-helper/src/sema4ai_http/__init__.py
@@ -199,151 +199,6 @@ class ResponseWrapper:
                 f"HTTP {self.response.status}: {self.response.reason}"
             )
 
-    def ok(self) -> bool:
-        return 200 <= self.response.status < 400
-
-
-class SSLContextFactory:
-    def __init__(self, config: dict | None = None) -> None:
-        self.config = config or {}
-
-    def _build_ssl_context(
-        self,
-        protocol: int = ssl.PROTOCOL_TLS_CLIENT,
-        enable_legacy_server_connect: bool | None = None,
-    ) -> ssl.SSLContext:
-        import truststore
-
-        ssl_context = truststore.SSLContext(protocol)
-
-        self._configure_ssl_verification(ssl_context)
-
-        if enable_legacy_server_connect:
-            self._configure_tls_renegociation(ssl_context)
-
-        return ssl_context
-
-    def _configure_ssl_verification(self, ssl_context: ssl.SSLContext) -> None:
-        disable_ssl = self.config.get("disable-ssl", False)
-        ssl_context.verify_mode = ssl.CERT_NONE if disable_ssl else ssl.CERT_REQUIRED
-
-    def _configure_tls_renegociation(self, ssl_context: ssl.SSLContext) -> None:
-        if sys.version_info < (3, 12):
-            _SSL_LEGACY_SERVER_CONNECT = 0x4
-        else:
-            _SSL_LEGACY_SERVER_CONNECT = ssl.OP_LEGACY_SERVER_CONNECT
-        ssl_context.options |= _SSL_LEGACY_SERVER_CONNECT
-
-
-class NetworkConfig:
-    def __init__(self) -> urllib3.PoolManager | urllib3.ProxyManager:
-        self.connection_pool = self._build_connection_pool()
-
-    @staticmethod
-    def _get_network_settings_path() -> Path:
-        if sys.platform == "win32":
-            localappdata = os.environ.get("LOCALAPPDATA")
-            if not localappdata:
-                raise RuntimeError("Error. LOCALAPPDATA not defined in environment!")
-            sema4_home = Path(localappdata) / "sema4ai"
-        else:
-            # Linux/Mac
-            sema4_home = Path("~/.sema4ai").expanduser()
-
-        return sema4_home / "network-settings.yaml"
-
-    def _get_current_config_profile(self) -> dict:
-        config_file = self._get_network_settings_path()
-
-        if config_file.exists():
-            import yaml
-
-            config = yaml.safe_load(config_file.read_text())
-            current_profile = config.get("current-profile", "")
-            return next(
-                (p for p in config.get("profiles", []) if p["name"] == current_profile),
-                {},
-            )
-
-        return {}
-
-    # TODO: add support for no-proxy setting
-    def _build_connection_pool(self) -> urllib3.PoolManager | urllib3.ProxyManager:
-        profile_config = self._get_current_config_profile()
-
-        ssl_context_factory = SSLContextFactory(profile_config)
-        ssl_context = ssl_context_factory._build_ssl_context(ssl.PROTOCOL_TLS_CLIENT)
-
-        if "proxy-settings" in profile_config:
-            proxy_settings = profile_config["proxy-settings"]
-            self.connection_pool = urllib3.ProxyManager(
-                proxy_url=proxy_settings.get("https-proxy")
-                or proxy_settings.get("http-proxy"),
-                ssl_context=ssl_context,
-            )
-        else:
-            self.connection_pool = urllib3.PoolManager(ssl_context=ssl_context)
-
-        return self.connection_pool
-
-
-@lru_cache
-def get_network_config() -> NetworkConfig:
-    return NetworkConfig().connection_pool
-
-
-def build_ssl_context(
-    protocol: int | None = None, *, enable_legacy_server_connect: bool | None = None
-) -> "ssl.SSLContext":
-    return SSLContextFactory({})._build_ssl_context(
-        protocol, enable_legacy_server_connect
-    )
-
-
-class ResponseWrapper:
-    def __init__(self, response: urllib3.BaseHTTPResponse):
-        self.response = response
-
-    @property
-    def status_code(self) -> int:
-        return self.response.status
-
-    @property
-    def text(self) -> str:
-        charset = "utf-8"
-        content_type = self.response.headers.get("content-type", "")
-        if "charset=" in content_type:
-            charset = content_type.split("charset=")[-1].strip()
-        return self.response.data.decode(charset, errors="replace")
-
-    @property
-    def data(self) -> bytes:
-        return self.response.data
-
-    def read(self, amt=None, decode_content=None, cache_content=False) -> bytes:
-        return self.response.read(amt, decode_content, cache_content)
-
-    def json(self) -> any:
-        return self.response.json()
-
-    def release_conn(self) -> None:
-        self.response.release_conn()
-
-    def getheaders(self) -> urllib3.response.HTTPHeaderDict:
-        return self.response.getheaders()
-
-    def close(self) -> None:
-        self.response.close()
-
-    def raise_for_status(self) -> None:
-        if self.response.status >= 400:
-            raise urllib3.exceptions.HTTPError(
-                f"HTTP {self.response.status}: {self.response.reason}"
-            )
-
-    def ok(self) -> bool:
-        return 200 <= self.response.status < 400
-
 
 class DownloadStatus(Enum):
     """Status"""
@@ -354,12 +209,6 @@ class DownloadStatus(Enum):
     HTTP_ERROR = auto()
 
 
-<<<<<<< HEAD
-connection_manager = NetworkConfig().connection_pool
-
-
-=======
->>>>>>> master
 def get(
     url,
     /,
@@ -369,10 +218,6 @@ def get(
     json: typing.Any | None = None,
     **urlopen_kw: typing.Any,
 ) -> ResponseWrapper:
-<<<<<<< HEAD
-    return ResponseWrapper(
-        connection_manager.request(
-=======
     """
     Perform a GET request using urllib3.
 
@@ -381,7 +226,6 @@ def get(
     """
     return ResponseWrapper(
         _get_connection_manager().request(
->>>>>>> master
             "get",
             url,
             body=body,
@@ -402,10 +246,6 @@ def post(
     json: typing.Any | None = None,
     **urlopen_kw: typing.Any,
 ) -> ResponseWrapper:
-<<<<<<< HEAD
-    return ResponseWrapper(
-        connection_manager.request(
-=======
     """
     Perform a POST request using urllib3.
 
@@ -414,7 +254,6 @@ def post(
     """
     return ResponseWrapper(
         _get_connection_manager().request(
->>>>>>> master
             "post",
             url,
             body=body,
@@ -435,10 +274,6 @@ def put(
     json: typing.Any | None = None,
     **urlopen_kw: typing.Any,
 ) -> ResponseWrapper:
-<<<<<<< HEAD
-    return ResponseWrapper(
-        connection_manager.request(
-=======
     """
     Perform a PUT request using urllib3.
 
@@ -447,7 +282,6 @@ def put(
     """
     return ResponseWrapper(
         _get_connection_manager().request(
->>>>>>> master
             "put",
             url,
             body=body,
@@ -468,10 +302,6 @@ def patch(
     json: typing.Any | None = None,
     **urlopen_kw: typing.Any,
 ) -> ResponseWrapper:
-<<<<<<< HEAD
-    return ResponseWrapper(
-        connection_manager.request(
-=======
     """
     Perform a PATCH request using urllib3.
 
@@ -480,7 +310,6 @@ def patch(
     """
     return ResponseWrapper(
         _get_connection_manager().request(
->>>>>>> master
             "patch",
             url,
             body=body,
@@ -501,10 +330,6 @@ def delete(
     json: typing.Any | None = None,
     **urlopen_kw: typing.Any,
 ) -> ResponseWrapper:
-<<<<<<< HEAD
-    return ResponseWrapper(
-        connection_manager.request(
-=======
     """
     Perform a DELETE request using urllib3.
 
@@ -513,7 +338,6 @@ def delete(
     """
     return ResponseWrapper(
         _get_connection_manager().request(
->>>>>>> master
             "delete",
             url,
             body=body,
@@ -766,11 +590,7 @@ def download_with_resume(
         target_path = target_path / filename
 
     if pool_manager is None:
-<<<<<<< HEAD
-        pool_manager = connection_manager
-=======
         pool_manager = _get_connection_manager()
->>>>>>> master
 
     request_info = _RequestInfo(
         url=url,
