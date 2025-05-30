2cfe98df379fd1e9da5665a7fa326819c3ad97ea
Ovidiu Rusu <ovidiu@sema4.ai>
2025-05-30 16:11:12 +0300

    Add dev-tasks to all templates

---
+++ templates/basic/tests/test_template.py
@@ -0,0 +1,4 @@
+def test_actions() -> None:
+    from actions import get_wikipedia_article_summary  # noqa
+
+    assert 1 == 1, "TODO: Add your tests here"
---
+++ templates/minimal/tests/test_template.py
@@ -0,0 +1,4 @@
+def test_actions() -> None:
+    from actions import greet  # noqa
+
+    assert 1 == 1, "TODO: Add your tests here"
