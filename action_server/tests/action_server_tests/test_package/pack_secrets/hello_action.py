2d2a478fafb2f111777bc01495a1ba244bc63bc3
Ovidiu Rusu <ovidiumax1994@gmail.com>
2025-10-09 14:39:12 +0300

    Add tests for tagged secret and guides (#280)
    * Add tests for tagged secret and guides

--- action_server/tests/action_server_tests/test_package/pack_secrets/hello_action.py
+++ action_server/tests/action_server_tests/test_package/pack_secrets/hello_action.py
@@ -1,16 +1,30 @@
-from sema4ai.actions import Secret, action
+from typing import Annotated
+
+from sema4ai.actions import Secret, SecretSpec, action
+
+AnnotatedSecret = Annotated[Secret, SecretSpec(tag="special-secret")]
+DocumentIntelligenceSecret = Annotated[Secret, SecretSpec(tag="document-intelligence")]
 
 
 @action(display_name="Display hello greeting")
-def hello_greeting(name: str, private_info: Secret) -> str:
+def hello_greeting(
+    name: str,
+    private_info: Secret,
+    annotated_secret: AnnotatedSecret,
+    doc_int_know_secret: DocumentIntelligenceSecret,
+) -> str:
     """
     Provides a greeting for a person.
 
     Args:
         name: The name of the person to greet.
         private_info: Some private information gotten from somewhere.
+        annotated_secret: Some private information gotten from somewhere with a special tag.
+
+    Note:
+        DocumentIntelligenceSecret type alias doesn't require docstrings as it is a know secret.
 
     Returns:
         The greeting for the person.
     """
-    return f"Hello {name}. Private info: {private_info.value}"
+    return f"Hello {name}. Private info: {private_info.value}. Annotated secret: {annotated_secret.value}"
