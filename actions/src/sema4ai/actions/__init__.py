1b23f8b02328197dc89517da73d94efce2991d9c
Fabio Zadrozny <fabiofz@gmail.com>
2025-11-04 08:35:32 +0100

    Release sema4ai-actions 1.6.2

--- actions/src/sema4ai/actions/__init__.py
+++ actions/src/sema4ai/actions/__init__.py
@@ -42,7 +42,7 @@ from ._response import ActionError, Response
 from ._secret import OAuth2Secret, Secret, SecretSpec
 from ._table import Row, RowValue, Table
 
-__version__ = "1.6.1"
+__version__ = "1.6.2"
 version_info = [int(x) for x in __version__.split(".")]
 
 
