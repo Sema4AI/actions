b313cfd6c68c83bce680843a629c2bd5bab09d47
Kari Harju <kari@sema4.ai>
2026-01-08 22:47:55 +0200

    RCC v21.0.0

--- action_server/src/sema4ai/action_server/_download_rcc.py
+++ action_server/src/sema4ai/action_server/_download_rcc.py
@@ -7,7 +7,7 @@ from typing import Optional
 log = logging.getLogger(__name__)
 
 # Note: also referenced in action_server/build.py
-RCC_VERSION = "20.3.3"
+RCC_VERSION = "21.0.0"
 
 
 def get_default_rcc_location() -> Path:
